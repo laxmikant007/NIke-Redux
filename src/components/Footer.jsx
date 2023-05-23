df0cbd1999e3b502e437ef3e51ca4228afab4321
LAXMIKANT SARASWAT <laxmikantsarswat007@gmail.com>
2023-05-23 23:54:45 +0530

    final footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,10 @@ function Footer() {
   return (
     <>
       <footer>
-        <p>A Quailty Product By Laxikant  😎</p>
-        <a href="https://laxmikant.co/">Visit Profile!!</a>
+        <p>A Quailty Product By Laxmikant Saraswat 😎
+        <a style={{color:'white', marginLeft:30 }} target="_blank" href="https://laxmikant.co/">Visit Profile!!</a>
+         </p>
+        
       </footer>
     </>
   );
